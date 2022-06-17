f2b9d08a32499f5be3d7a0dadd65cd9c1ce5dfb1
Bjarne Andersson <bjarne4andersson@gmail.com>
2022-06-17 19:30:21 +0200

    :sparkles: screen_position_to_position dict

--- src/Helpers/Converters.py
+++ src/Helpers/Converters.py
@@ -2,3 +2,9 @@ def db_position_to_position(db_position: str) -> dict:
     # (6,4)
     column, row = position = db_position.replace("(", "").replace(")", "").split(",")
     return {'column': int(column), 'row': int(row)}
+
+
+def screen_position_to_position(screen_position: str) -> dict:
+    # (60,40) -> {'x': 6, 'y': 4}
+    x, y = position = screen_position.replace("(", "").replace(")", "").split(",")
+    return {'x': int(x), 'y': int(y)}
